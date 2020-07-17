56cf13b9b2a1dc3c8f0f8d7e09674d9eb649301a
吴诗雅 <wsy_0301@hotmail.com>
2020-07-17 15:12:57 +0800

    完成登录页注册页渲染

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,107 @@
-import React,{Component} from 'react';
-import Tabbar from '@/components/Tabbar'
-class Login extends Component{
-    render(){
-        return(
-            <div>Login
-                <Tabbar/>
-            </div>
-        )
-    }
-}
+import React,{Component, useState} from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { NavBar, Icon } from 'antd-mobile';
+import { UserOutlined, LockOutlined,HomeOutlined,AppstoreOutlined,ShoppingCartOutlined } from '@ant-design/icons';
+import './login.scss'
+// import Tabbar from '@/components/Tabbar'
+// class Login extends Component{
+//     render(){
+//         return(
+//             <div>Login
+//                 {/* <Tabbar/> */}
+//             </div>
+//         )
+//     }
+// }
 
+function Login (props){
+    const [isShow,show] = useState(false)
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+    
+    const toPage = (path)=>{
+        props.history.push(path)
+    }
+    return (
+        <div className="login">
+            <NavBar
+                style={{height:50}}
+                mode="light"
+                icon={<Icon type="left" style={{color:'#333'}}/>}
+                onLeftClick={() => props.history.go(-1)}
+                rightContent={[
+                    <Icon key="0" type="ellipsis" style={{color:'#333'}} onClick={()=>{
+                        show(!isShow)
+                    }}/>,
+                ]}
+                >登录
+            </NavBar>
+             {/* 点击出现新导航 */}
+            <nav style={{display:`${isShow?'block':'none'}`}}>
+                <li className="navlist" onClick={()=>{toPage('/home')}}>
+                    <HomeOutlined />
+                    <span>首页</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/sort')}}>
+                    <AppstoreOutlined />
+                    <span>分类</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/cart')}}>
+                    <ShoppingCartOutlined />
+                    <span>购物车</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/mine')}}>
+                    <UserOutlined />
+                    <span>我的e宠</span>
+                </li>
+            </nav>
+            {/* 登录框 */}
+            <div className="login_box">
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                        {
+                            required: true,
+                            message: '请输入用户名!',
+                        },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                        {
+                            required: true,
+                            message: '请输入密码!',
+                        },
+                        ]}
+                    >
+                        <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="密码"
+                        />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                        登录
+                        </Button>
+                        &nbsp;&nbsp;Or <span className="to_reg" onClick={()=>{toPage('/reg')}}>现在注册!</span>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
 export default Login
--- src/pages/Reg/index.js
+++ src/pages/Reg/index.js
@@ -1,11 +1,141 @@
-import React,{Component} from 'react';
+import React,{Component,useState} from 'react';
+import { NavBar, Icon } from 'antd-mobile';
+import { UserOutlined,HomeOutlined,AppstoreOutlined,ShoppingCartOutlined } from '@ant-design/icons';
+import { Form, Input, Button } from 'antd';
+import './reg.scss'
 
-class Reg extends Component{
-    render(){
-        return(
-            <div>Reg</div>
-        )
+function Reg (props){
+    const [isShow,show] = useState(false)
+    const toPage = (path)=>{
+        props.history.push(path)
     }
+    // const formItemLayout = {
+    //     labelCol: {
+    //       xs: { span: 24 },
+    //       sm: { span: 8 },
+    //     },
+    //     wrapperCol: {
+    //       xs: { span: 24 },
+    //       sm: { span: 16 },
+    //     },
+    // };
+    const tailFormItemLayout = {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+    };
+    const [form] = Form.useForm();
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+    return(
+        <div className="reg">
+            <NavBar
+                style={{height:50}}
+                mode="light"
+                icon={<Icon type="left" style={{color:'#333'}}/>}
+                onLeftClick={() => props.history.go(-1)}
+                rightContent={[
+                    <Icon key="0" type="ellipsis" style={{color:'#333'}} onClick={()=>{
+                        show(!isShow)
+                    }}/>,
+                ]}
+                >注册
+            </NavBar>
+             {/* 点击出现新导航 */}
+            <nav style={{display:`${isShow?'block':'none'}`}}>
+                <li className="navlist" onClick={()=>{toPage('/home')}}>
+                    <HomeOutlined />
+                    <span>首页</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/sort')}}>
+                    <AppstoreOutlined />
+                    <span>分类</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/cart')}}>
+                    <ShoppingCartOutlined />
+                    <span>购物车</span>
+                </li>
+                <li className="navlist" onClick={()=>{toPage('/mine')}}>
+                    <UserOutlined />
+                    <span>我的e宠</span>
+                </li>
+            </nav>
+            {/* 注册框 */}
+            <Form
+                form={form}
+                name="register"
+                onFinish={onFinish}
+                scrollToFirstError
+                className="reg_form"
+            >
+                {/* 用户名 */}
+                <Form.Item
+                    name="username"
+                    rules={[
+                    {
+                        required: true,
+                        message: '请输入用户名!',
+                    },
+                    ]}
+                    label="用户名"
+                >
+                    <Input />
+                </Form.Item>
+                {/* 密码 */}
+                <Form.Item
+                    name="password"
+                    label="密码"
+                    rules={[
+                    {
+                        required: true,
+                        message: '请输入密码!',
+                    },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+                {/* 确认密码 */}
+                <Form.Item
+                    name="confirm"
+                    label="确认密码"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                    {
+                        required: true,
+                        message: '请再次输入密码!',
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(rule, value) {
+                        if (!value || getFieldValue('password') === value) {
+                            return Promise.resolve();
+                        }
+                        return Promise.reject('两次输入密码不一致！');
+                        },
+                    }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item {...tailFormItemLayout} >
+                    <Button type="primary" htmlType="submit">
+                        注册
+                    </Button>
+                </Form.Item>
+            </Form>
+
+        </div>
+    )
 }
 
 export default Reg
