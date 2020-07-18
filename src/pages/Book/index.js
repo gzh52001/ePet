1ddb6f29828c35329b9a36f26b567886afa1b4f5
吴诗雅 <wsy_0301@hotmail.com>
2020-07-18 17:53:40 +0800

    修改登录状态下的 我的萌书

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -39,8 +39,10 @@ class Book extends Component{
             ],
             banner:[],
             column:[],
+            user:{}
         }
         this.getData = this.getData.bind(this)
+        this.getUser = this.getUser.bind(this)
     }
     async getData(){
         try{
@@ -48,17 +50,30 @@ class Book extends Component{
             console.log(p.data);
             this.setState({
                 banner:p.data.data.advs,
-                column:p.data.data.column
+                column:p.data.data.column,
             })
         }catch(err){
             console.log(err);
         }
     }
+    getUser(){
+        let res = localStorage.getItem('ep-username')
+        // console.log(JSON.parse(res));
+        if(res){
+            this.setState({
+                user:{
+                    username:JSON.parse(res)
+                }
+            })
+        }
+    }
     componentDidMount(){
+        this.getUser()
         this.getData()
+        
     }
     render(){
-        const {advice,banner,column} = this.state
+        const {advice,banner,column,user} = this.state
         return(
             <div className="book">
                 {/* 头部 */}
@@ -79,13 +94,25 @@ class Book extends Component{
                             <MessageOutlined style={{fontSize:20,marginLeft:8}}/>
                         </div>
                     </div>
-                    <div className="userInfo">
-                        <img src="https://static.epetbar.com/epet_wap_img/opgc/zms_avatar_dog.png"/>
-                        <div>
-                            <h3>立即登录</h3>
-                            <span>我们想要更懂你</span>
+                    {
+                        user.username ?
+                        <div className="userInfo">
+                            <img src="https://img2.epetbar.com/dogs/1.jpg"/>
+                            <div>
+                                <h3>添加宠物</h3>
+                                <span>定制养宠干货</span>
+                            </div>
                         </div>
-                    </div>
+                        :
+                        <div className="userInfo">
+                            <img src="https://static.epetbar.com/epet_wap_img/opgc/zms_avatar_dog.png"/>
+                            <div>
+                                <h3>立即登录</h3>
+                                <span>我们想要更懂你</span>
+                            </div>
+                        </div>
+                    }
+                    
                     
                 </div>
                 {/* 百科 */}
